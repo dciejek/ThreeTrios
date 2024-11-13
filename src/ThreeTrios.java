8f96a178ff12897c669fca0633e1be74fe8634c0
reecedigiacomo <digiacomo.r@northeastern.edu>
2024-11-13 16:43:00 -0500

    View

--- src/ThreeTrios.java
+++ src/ThreeTrios.java
@@ -1,17 +1,32 @@
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.util.List;
+
+import controller.FileHandler;
+import model.Cell;
 import model.PlayingCard;
 import model.TTModel;
 import model.ThreeTriosModel;
 import view.TTGuiView;
 import view.ThreeTriosFrame;
-import view.ThreeTriosView;
 
 /**
- * The placeholder class for our project, as described by HW6.
+ * The placeholder main class for our project, as described by HW6 displays
+ * the view as a moment in time as described by how the model is set up.
  */
 public final class ThreeTrios {
   public static void main(String[] args) {
     ThreeTriosModel<PlayingCard> model = new TTModel();
+    File cardsFile = new File("docs" + File.separator + "cards1");
+    File gridFile = new File("docs" + File.separator + "XGrid");
+    List<List<Cell<PlayingCard>>> grid = FileHandler.readGrid(gridFile);
+    List<PlayingCard> cards = FileHandler.readCards(cardsFile);
+    int rows = FileHandler.readRowNum(gridFile);
+    int cols = FileHandler.readColNum(gridFile);
+
+    model.startGame(grid, cards, rows, cols);
     ThreeTriosFrame view = new TTGuiView(model);
     view.makeVisible();
+    view.addClickListener();
   }
 }
