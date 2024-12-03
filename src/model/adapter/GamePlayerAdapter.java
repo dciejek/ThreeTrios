cc9fe0c3843c5c2c2fe7f5425d4d6d79cb1e7593
dciejek <ciejek.d@northeastern.edu>
2024-12-02 19:23:19 -0500

    Fixed Card

--- src/model/adapter/GamePlayerAdapter.java
+++ src/model/adapter/GamePlayerAdapter.java
@@ -1,4 +1,57 @@
 package model.adapter;
 
-public class GamePlayerAdapter {
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import model.Card;
+import model.Player;
+import model.PlayerColor;
+import model.ThreeTriosModel;
+import provider.model.GamePlayer;
+import provider.model.Model;
+import provider.model.Move;
+import strategy.Play;
+
+public final class GamePlayerAdapter implements GamePlayer, Player<Card> {
+  private final Player<Card> adaptee;
+
+  public GamePlayerAdapter(Player<Card> adaptee) {
+    this.adaptee = adaptee;
+  }
+
+  @Override
+  public PlayerColor getColor() {
+    return null;
+  }
+
+  @Override
+  public List getHand() {
+    return List.of();
+  }
+
+  @Override
+  public void removeFromHand(Card card) {
+
+  }
+
+  @Override
+  public void addToHand(Card card) {
+
+  }
+
+  @Override
+  public Play getPlay(ThreeTriosModel model) {
+    return null;
+  }
+
+  @Override
+  public void setModel(ThreeTriosModel model) {
+
+  }
+
+  @Override
+  public void accept(Consumer<Move> moveConsumer, Supplier<Model> supplier) {
+
+  }
 }
--- src/provider/model/Card.java
+++ src/provider/model/Card.java
@@ -1,62 +1,26 @@
 package provider.model;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
+import provider.model.CoachColor;
 
 /**
  * to represent a card in three trios.
  */
-public final class Card {
-
-  private final String name;
-  private final Map<CardinalDirection, AttackValue> attackValues;
-  private CoachColor coachColor;
-
-  /**
-   * to construct a card with [name] and [attackValues].
-   * @param name         - name of this card
-   * @param attackValues - attack value in each direction
-   */
-  public Card(String name, Map<CardinalDirection, AttackValue> attackValues) {
-    if (name == null) {
-      throw new IllegalArgumentException("card name cannot be null");
-    }
-    if (attackValues == null) {
-      throw new IllegalArgumentException("attack values cannot be null");
-    }
-    if (name.isEmpty()) {
-      throw new IllegalArgumentException("card name cannot be empty");
-    }
-    this.name = name;
-    this.attackValues = new HashMap<>(attackValues);
-  }
+public interface Card {
 
 
   /**
    * to return the name of the card.
    * @return - the name of the card.
    */
-  public String getName() {
-    return name;
-  }
+  String getName();
 
   /**
    * to return the coach of this card.
    * @return - the current coach of this card.
    */
-  public CoachColor getCoach() {
-    return this.coachColor;
-  }
-
-  /**
-   * to update the coach to [newCoach].
-   * @param newCoach - the new coach of this
-   */
-  void setCoach(CoachColor newCoach) {
-    this.coachColor = newCoach;
-  }
+  CoachColor getCoach();
 
   /**
    * to evaluate if this card beats [other] in [direction].
@@ -64,59 +28,13 @@ public final class Card {
    * @param direction - the direction where this battles other
    * @return the result of the battle.
    */
-  public boolean beats(Card other, CardinalDirection direction) {
-    return getAttackValue(direction).beats(other.getAttackValue(direction.opposite()));
-  }
+  boolean beats(Card other, CardinalDirection direction);
 
   /**
    * to return the attack value of this card in [direction].
    * @param direction - the direction to check
    * @return - the attack value in [direction]
    */
-  public AttackValue getAttackValue(CardinalDirection direction) {
-    return attackValues.get(direction);
-  }
-
-  @Override
-  public int hashCode() {
-    return toString().hashCode();
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj instanceof Card) {
-      Card other = (Card) obj;
-      return this.toString().equals(other.toString())
-          && this.coachColor == other.coachColor;
-    } else {
-      return false;
-    }
-  }
-
-  // package private to prevent excess side-effect
-
-  @Override
-  public String toString() {
-    String s = name;
-    for (model.CardinalDirection c : model.CardinalDirection.values()) {
-      s += " " + attackValues.get(c).toString();
-    }
-    return s;
-  }
-
-  /**
-   * to set the attack value in [direction] to [av].
-   * @param av        - the attack value to add
-   * @param direction - the direction to update
-   */
-  void setAttackValueInDirection(AttackValue av, CardinalDirection direction) {
-    attackValues.put(direction, av);
-  }
-
-  Card copy() {
-    Card card = new Card(name, Collections.unmodifiableMap(attackValues));
-    card.setCoach(this.coachColor);
-    return card;
-  }
+  AttackValue getAttackValue(CardinalDirection direction);
 
 }
